691531bd3fd4b958281e21555d6ecf11b41951df
Mikhail <m6453327@icloud.com>
2025-05-23 02:12:39 +0300

    initial commit

---
+++ src/main/java/com/trackmyhabits/TrackMyHabitsApplication.java
@@ -0,0 +1,13 @@
+package com.trackmyhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyHabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyHabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackmyhabits/TrackMyHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmyhabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyHabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
