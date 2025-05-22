360bb4719006b18ea17642f71f6488878f942103
Mikhail <m6453327@icloud.com>
2025-05-23 02:27:46 +0300

    disabled tests

--- src/test/java/com/trackmyhabits/TrackMyHabitsApplicationTests.java
+++ src/test/java/com/trackmyhabits/TrackMyHabitsApplicationTests.java
@@ -1,8 +1,10 @@
 package com.trackmyhabits;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class TrackMyHabitsApplicationTests {
 
